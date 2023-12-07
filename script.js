43badc4ae2d810bec73a9ae2d04c537768be2fc0
Frida Fernhede <fernhedefrida@gmail.com>
2023-12-07 09:58:24 +0100

    Changed names of some style variables

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-    const swiper = new Swiper('.swiperMovie', {
-        speed: 400,
-        spaceBetween: 100,
-        autoplay: {
-            delay: 5000,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            // When window width is <= 767px
-            767: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-            },
-            // When window width is <= 991px
-            991: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-            },
-            // When window width is <= 1199px
-            1199: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-            },
-        },
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiperMovie", {
+    speed: 400,
+    spaceBetween: 100,
+    autoplay: {
+      delay: 5000,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      // When window width is <= 767px
+      767: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      // When window width is <= 991px
+      991: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // When window width is <= 1199px
+      1199: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
 });
-const indexswiper = new Swiper(".indexSwiper", {
+const indexSwiper = new Swiper(".indexSwiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
-  //add navitagion om bredare än 900px?
   breakpoints: {
     200: {
       slidesPerView: 1.5,
@@ -51,8 +49,5 @@ const indexswiper = new Swiper(".indexSwiper", {
       slidesPerView: 4.5,
       spaceBetween: 0,
     },
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
   },
 });
